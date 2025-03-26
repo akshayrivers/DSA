b7578ed05942407c8c873cc30184ffa4986cf292
Vinod Akshat <pumkininriver@gmail.com>
2025-03-26 23:18:21 +0530

    Quick Sort implementation in C++

---
+++ C++/Basic/quick_sort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/**
+ * Multiple ways to partition the array:
+ * 1. naive approach 
+ * 2. lumuto paritition
+ * 3. Hoarse's partition
+ */
+// int[] partition_nav(int[]& arr){
+
+// }
+// int[] partition_lumuto(int[]& arr){
+    
+// }
+// int[] partition_hoarse(int[]& arr){
+    
+// }
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
+}
+void quick_sort(vector<int>& arr, int low, int high) {//avg: O(nlogn) worst: O(n^2)
+    if (low < high) {
+      
+        int pi = partition(arr, low, high);
+
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+
+}
+int main() {
+
+    vector<int> arr = {5,4,3,6,7,9,0,1,2}; 
+    quick_sort(arr, 0, arr.size() - 1);
+    cout<<"Sorted Array: ";
+    for (int num : arr) cout << num << " ";
+
+
+
+    return 0;
+}
