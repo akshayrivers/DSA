47b21de578ac96c5af70eb7eb05311a0bb38c69a
Vinod Akshat <pumkininriver@gmail.com>
2025-07-01 16:44:25 +0530

    go pickachu I choose you

--- C++/Graph/kruskal.cpp
+++ C++/Graph/kruskal.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include<algorithm>
-#include <stack>
-#include <numeric>
+#include <algorithm>
 using namespace std;
 
 struct Edge {
@@ -11,74 +8,53 @@ struct Edge {
     Edge(int s, int d, int w) : src(s), des(d), wt(w) {}
 };
 
-// ----------- Disjoint Set (Union-Find) -----------
-class DSU {
-    vector<int> parent, rank;
-
-public:
-    DSU(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-        iota(parent.begin(), parent.end(), 0);
-    }
-
-    int find(int x) {
-        if (x != parent[x])
-            parent[x] = find(parent[x]); // path compression
-        return parent[x];
-    }
-
-    bool unite(int x, int y) {
-        int px = find(x), py = find(y);
-        if (px == py) return false;
-
-        // union by rank
-        if (rank[px] < rank[py]) {
-            parent[px] = py;
-        } else if (rank[px] > rank[py]) {
-            parent[py] = px;
-        } else {
-            parent[py] = px;
-            rank[px]++;
-        }
-        return true;
-    }
-};
-
-// -------------- Kruskal’s Algorithm ----------------
-void kruskalMST(vector<Edge> &edges, int V) {
-    sort(edges.begin(), edges.end(), [](Edge &a, Edge &b) {
-        return a.wt < b.wt;
-    });
-
-    DSU dsu(V);
-    int mstCost = 0;
-    vector<Edge> mst;
-
-    for (Edge &e : edges) {
-        if (dsu.unite(e.src, e.des)) {
-            mst.push_back(e);
-            mstCost += e.wt;
-        }
-    }
-
-    cout << "Edges in MST (Kruskal):\n";
-    for (auto &e : mst) {
-        cout << e.src << " - " << e.des << " @ " << e.wt << "\n";
-    }
-    cout << "Total cost: " << mstCost << "\n";
+bool cmp(Edge &a, Edge &b) {
+    return a.wt < b.wt;
 }
+
+vector<int> parent;
+
+int find(int x) {
+    if (parent[x] != x)
+        parent[x] = find(parent[x]); // path compression
+    return parent[x];
+}
+
+void unite(int a, int b) {
+    int pa = find(a);
+    int pb = find(b);
+    if (pa != pb) {
+        parent[pa] = pb;
+    }
+}
+
+void kruskal(int V, vector<Edge> &edges) {
+    sort(edges.begin(), edges.end(), cmp);
+    parent.resize(V);
+    for (int i = 0; i < V; i++) parent[i] = i;
+
+    int mstCost = 0;
+    cout << "Edges in MST:\n";
+    for (auto &e : edges) {
+        if (find(e.src) != find(e.des)) {
+            unite(e.src, e.des);
+            mstCost += e.wt;
+            cout << e.src << " - " << e.des << " @ " << e.wt << "\n";
+        }
+    }
+    cout << "Total MST cost: " << mstCost << endl;
+}
+
 int main() {
-    int V = 5; // number of vertices
-    vector<Edge> edges;
+    int V = 5;
+    vector<Edge> edges = {
+        {0, 1, 10},
+        {0, 2, 15},
+        {1, 3, 40},
+        {2, 3, 50},
+        {3, 4, 20}
+    };
 
-    // Add undirected edges
-    edges.push_back(Edge(0, 1, 10));
-    edges.push_back(Edge(0, 2, 6));
-    edges.push_back(Edge(0, 3, 5));
-    edges.push_back(Edge(1, 3, 15));
-    edges.push_back(Edge(2, 3, 4));
-
-    kruskalMST(edges, V);
+    kruskal(V, edges);
     return 0;
 }
